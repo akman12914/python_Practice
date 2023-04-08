c153c24daa967995fcf325258f044085976e9a61
akman12914 <rnlduadns98@naver.com>
2023-04-08 23:48:22 +0900

    기초자료형 - 기본 계산기 실습

---
+++ 기초자료형/calculator.py
@@ -0,0 +1,7 @@
+firstNumber = int(input("첫 번째 숫자를 입력하세요:"));
+secondNumber = int(input("두 번째 숫자를 입력하세요:"));
+
+print(firstNumber, "+" , secondNumber, "=", (firstNumber + secondNumber));
+print(firstNumber, "-" , secondNumber, "=", (firstNumber - secondNumber));
+print(firstNumber, "*" , secondNumber, "=", (firstNumber * secondNumber));
+print(firstNumber, "/" , secondNumber, "=", (firstNumber / secondNumber));
